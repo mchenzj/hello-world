d3d214470791fdc0b2148fb2ec5a53a224f5714a
mchenzj <24836606@qq.com>
2018-08-06 11:18:32 +0800

    新增stockID属性

--- com/esquel/stockout/StockOut.java
+++ com/esquel/stockout/StockOut.java
@@ -1,5 +1,17 @@
 package com.esquel.stockout;
 
 public class StockOut {
+	
+	private String stockID;
+
+	public String getStockID() {
+		return stockID;
+	}
+
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+	
+	
 
 }
