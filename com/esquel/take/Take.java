9387633cb398bebb18cf0b19600c418e27203e23
mchenzj <24836606@qq.com>
2018-08-06 16:47:27 +0800

    新增id

--- com/esquel/take/Take.java
+++ com/esquel/take/Take.java
@@ -1,5 +1,17 @@
 package com.esquel.take;
 
 public class Take {
+	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 
 }
